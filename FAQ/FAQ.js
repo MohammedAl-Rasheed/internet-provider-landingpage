f5550c1c26e5fd4b172bafed60503f556ee39b65
MohammedAl-Rasheed <mohammed.satar.alrasheed@gmail.com>
2020-10-05 21:01:29 -0400

    first commit

---
+++ FAQ/FAQ.js
@@ -0,0 +1,22 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+    const itemToggle = this.getAttribute('aria-expanded');
+
+    for (i = 0; i < items.length; i++) {
+        items[i].setAttribute('aria-expanded', 'false');
+    }
+
+    if (itemToggle == 'false') {
+        this.setAttribute('aria-expanded', 'true');
+    }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
